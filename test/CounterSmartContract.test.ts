b4c881104021a146fa38b843c5f28cbf49286efe
Berzan <berzan@tutanota.com>
2024-02-27 22:50:51 +0300

    create: counter smart contract test

---
+++ test/CounterSmartContract.test.ts
@@ -0,0 +1,22 @@
+import assert from "node:assert"
+import { describe, it } from "node:test"
+import { AccountUpdate, Mina, PrivateKey, UInt64 } from "o1js"
+import { CounterSmartContract } from "../src/CounterSmartContract.js"
+
+describe("YourSmartContract", async () => {
+	it("can deploy the smart contract", async () => {
+		assert.fail()
+	})
+
+	it("can increment the count", async () => {
+		assert.fail()
+	})
+
+	it("can decrement the count", async () => {
+		assert.fail()
+	})
+
+	it("can set the count to 42", async () => {
+		assert.fail()
+	})
+})
