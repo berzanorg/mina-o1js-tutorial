6424e6a93d4d3de2b051b6643f1a27d715a3bf03
Berzan <berzan@tutanota.com>
2024-02-27 22:49:51 +0300

    create: counter zk program test

---
+++ test/CounterZkProgram.test.ts
@@ -0,0 +1,22 @@
+import assert from "node:assert"
+import { describe, it } from "node:test"
+import { UInt64, verify } from "o1js"
+import { CounterZkProgram } from "../src/CounterZkProgram.js"
+
+describe("YourZkProgram", async () => {
+	it("can increment the count", async () => {
+		assert.fail()
+	})
+
+	it("can decrement the count", async () => {
+		assert.fail()
+	})
+
+	it("can set the count to 42", async () => {
+		assert.fail()
+	})
+
+	it("can verify the last proof with verification key", async () => {
+		assert.fail()
+	})
+})
